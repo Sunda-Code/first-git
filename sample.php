dae72c1a118ccae4338a08db2521be1bf2b11c5e
taro.kirameki <taro.kirameki@techacademy.jp>
2021-02-20 07:21:01 +0000

    add sample.php

--- sample.php
+++ sample.php
@@ -1,14 +1,3 @@
-<?php
-    $result = '無し';
-    if (array_key_exists('act', $_POST)) {
-        $result = omikuji();
-    }
-    
-    function omikuji() {
-        $fortune = ["大吉","吉","中吉","小吉","末吉","凶"];
-        return $fortune[randaom_int(0, count($fortune) -1)];
-    }
-?>
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -17,7 +6,7 @@
     </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果；無し</p>
+        <p>おみくじの結果：無し</p>
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
